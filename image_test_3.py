1d213b8f5fa20b4dcf323e9cfbd7650e583b7832
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-03-11 18:52:04 -0600

    Working ohlc bars and moving average line.

--- image_test_3.py
+++ image_test_3.py
@@ -1,4 +1,3 @@
-# TODO: Make this work for each day moving forward in dataset.
 # TODO: Get CRSP data
 # TODO: Rework to work with CRSP data
 # TODO: Generate volume bars
@@ -8,81 +7,177 @@
 import data_utils as du
 import polars as pl
 import matplotlib.pyplot as plt
+from datetime import date
+from tqdm import tqdm
+import numpy as np
 
 pl.enable_string_cache()
 
-N_DAYS = 60 # * 3 = width of image
-NUM_BINS = N_DAYS * 2 # Height of the image (* num_tickers = height of dataframe)
+def get_period_pairs(dates: pl.DataFrame, look_back: int) -> list[tuple[date, date]]:
+    return (
+        dates.with_columns(pl.col("date").shift(look_back - 1).alias("start"))
+        .select("start", pl.col("date").alias("end"))
+        .drop_nulls()
+        .rows()
+    )
 
-labels = [f"{i}" for i in range(NUM_BINS)]
-
-tickers = ['AAPL', 'WMT', 'NVDA']
-df = du.load_yfinance(tickers)
-
-def get_bulk_images(df: pl.DataFrame) -> pl.DataFrame:
+def generate_ohlc_bars(df: pl.DataFrame, n_days: int, n_bins: int) -> np.array:
+    labels = [f"{i}" for i in range(n_bins)]
+    index = pl.DataFrame({'bin': [str(i) for i in range(n_bins)]})
     df = (
         df
-        # Get last n_days * n_tickers
-        .tail(N_DAYS * len(df['ticker'].unique()))
         # Create index column
-        .with_columns(
-            pl.col('ticker').cum_count().over("ticker").alias("index")     
-        )
+        .with_row_index("index", 1)
         # Unpivot OHLC variables
-        .unpivot(on=['open', 'high', 'low', 'close'], index=['date', 'index', 'ticker'])
-        .sort(['date', 'ticker', 'variable'])
+        .unpivot(on=["open", "high", "low", "close"], index="index")
+        .sort(["index", "variable"])
         # Put variable values into bins for the whole sample by ticker
         .with_columns(
-            pl.col('value').qcut(NUM_BINS, labels=labels, allow_duplicates=True).over('ticker').alias('bin'),
-            pl.lit(1).alias('in_bin')
+            pl.col("value")
+            .qcut(n_bins, labels=labels, allow_duplicates=True)
+            .alias("bin"),
+            pl.lit(1).alias("in_bin"),
         )
-        .sort('bin')
+        .sort("bin")
         # Pivot to bins wide
-        .pivot(on='bin', index=['index', 'ticker', 'variable'], values='in_bin')
-        .sort('index', 'ticker', 'variable')
+        .pivot(on="bin", index=["index", "variable"], values="in_bin")
+        .sort("index", "variable")
         # Concatenate high and low together
         .with_columns(
-            pl.col('variable').replace({'high': 'high_low', 'low': 'high_low'})
+            pl.col("variable").replace({"high": "high_low", "low": "high_low"})
         )
         # Create x axis column
         .with_columns(
-            pl.concat_str(pl.col('index'), pl.col('variable'), separator='_').alias('x')
+            pl.concat_str(pl.col("index"), pl.col("variable"), separator="_").alias("x")
         )
-        .drop('index', 'variable')
+        .drop("index", "variable")
         # Unpivot and pivot to x and y axes
-        .unpivot(index=['ticker', 'x'], variable_name='bin', value_name='in_bin')
-        .pivot(on='x', index=['ticker', 'bin'], values='in_bin', aggregate_function='max')
+        .unpivot(index="x", variable_name="bin", value_name="in_bin")
+        .pivot(on="x", index="bin", values="in_bin", aggregate_function="max")
     )
-
-    return df
-
-def get_image_from_bulk_images(df: pl.DataFrame, ticker: str) -> pl.DataFrame:
     df = (
-        df
-        # Filter to single ticker
-        .filter(pl.col('ticker').eq(ticker))
-        # Sort y axis by bins descending (high price at top)
-        .cast({'bin': pl.Int32})
-        .sort('bin', descending=True)
+        index
+        .join(df, on='bin', how='left')
+        .cast({"bin": pl.Int32})
+        .sort("bin", descending=True)
         # Interpolate vertical missing values
         .interpolate()
         .fill_null(0)
         # Reorder x axis to be by date open, high_low, close
-        .select(['bin'] + [f"{i + 1}_{var}" for i in range(N_DAYS) for var in ['open', 'high_low', 'close']])
+        .select(
+            [
+                f"{i + 1}_{var}"
+                for i in range(n_days)
+                for var in ["open", "high_low", "close"]
+            ]
+        )
+        .to_numpy()
     )
 
     return df
 
-# Get bulk images
-df = get_bulk_images(df)
-print(df)
+def generate_ma_line(df: pl.DataFrame, n_days: int, n_bins: int) -> np.array:
+    labels = [f"{i}" for i in range(n_bins)]
+    min_value = df['low'].min()
+    max_value = df['high'].max()
+    breaks = np.linspace(min_value, max_value, n_bins + 2)[1:n_bins]
+    index = pl.DataFrame({'bin': [str(i) for i in range(n_bins)]})
+    df = (
+        df
+        .with_row_index('index', 1)
+        .with_columns(pl.lit(None).alias('a'), pl.lit(None).alias('z'))
+        .unpivot(index='index', on=['a', 'ma', 'z'])
+        .sort('index', 'variable')
+        .with_columns(pl.col('value').interpolate())
+        .fill_null(strategy='forward')
+        .fill_null(strategy='backward')
+        .with_columns(
+            pl.col("value")
+            .cut(breaks=breaks, labels=labels)
+            .alias("bin"),
+            pl.lit(1).alias("in_bin"),
+        )
+        .sort("bin")
+        # Pivot to bins wide
+        .pivot(on="bin", index=["index", "variable"], values="in_bin")
+        .sort("index", "variable")
+        # Create x axis column
+        .with_columns(
+            pl.concat_str(pl.col("index"), pl.col("variable"), separator="_").alias("x")
+        )
+        .drop("index", "variable")
+        # Unpivot and pivot to x and y axes
+        .unpivot(index="x", variable_name="bin", value_name="in_bin")
+        .pivot(on="x", index="bin", values="in_bin", aggregate_function="max")
+    )
+    df = (
+        index.join(df, on='bin', how='left')
+        .cast({"bin": pl.Int32})
+        .sort("bin", descending=True)
+                .interpolate()
+        .fill_null(0)
+        # Reorder x axis to be by date open, high_low, close
+        .select(
+            [
+                f"{i + 1}_{var}"
+                for i in range(n_days)
+                for var in ["a", "ma", "z"]
+            ]
+        )
+        .to_numpy()
+    )
 
-for ticker in tickers:
-    # Filter df by ticker and gen image
-    sub_df = get_image_from_bulk_images(df, ticker)
-    print(sub_df)
+    return df
 
-    # Plot image 
-    plt.imshow(sub_df.drop('bin'), cmap='grey')
-    plt.show()
-    plt.clf()
+def generate_volumes_bars(df: pl.DataFrame) -> np.array:
+
+    df = (
+        df.select(['date', 'volume'])
+    )
+
+    return df
+
+
+
+def generate_images(df: pl.DataFrame, look_back: int = 20, n_bins: int = 40) -> None:
+    tickers = df["ticker"].unique().sort()
+    dates = df.select("date").unique().sort("date")
+    periods = get_period_pairs(dates, look_back)
+
+    for ticker in tqdm(tickers, desc="Generating images...", position=0, disable=True):
+        # Subset on ticker
+        ticker_df = df.filter(pl.col("ticker").eq(ticker))
+
+        for start, end in tqdm(
+            periods[21:22], desc=f"Generating images for {ticker}", leave=False, position=1, disable=True
+        ):
+            # Subset on period
+            period_df = ticker_df.filter(pl.col("date").is_between(start, end))
+
+            # Generate image
+            ohlc_bars = generate_ohlc_bars(period_df, look_back, n_bins)
+            ma_line = generate_ma_line(period_df, look_back, n_bins)
+            volume_bars = generate_volumes_bars(period_df)
+            # print(ohlc_bars)
+            # print(ma_line)
+            print(volume_bars)
+            # print(ohlc_bars.shape)
+            # print(ma_line.shape)
+
+            image = np.maximum(ohlc_bars, ma_line)
+        
+            # Save
+            # plt.imshow(image, cmap='grey')
+            # plt.show()
+
+            plt.imsave(f"images/{ticker}_{end.strftime('%Y%m%d')}.png", image, cmap="grey")
+            break
+
+
+if __name__ == '__main__':
+    tickers = ["AAPL"]
+    df = (
+        du.load_yfinance(tickers)
+        .with_columns(pl.col('close').rolling_mean(window_size=20).alias('ma'))
+    )
+    generate_images(df, look_back=20, n_bins=40) # 60 x 40 image
